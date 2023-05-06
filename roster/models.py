33e8f4f4d607f4a67338af44cc43ec8a1d344623
spalme21 <spalme21@gmail.com>
2023-05-06 13:17:58 -0500

    Add Client, ClassType, and ClassSession models

--- roster/models.py
+++ roster/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your models here.
+class Client(models.Model):
+    """Model representing a client."""
+    last_name = models.CharField(max_length=32, help_text="Enter the last name")
+    first_name = models.CharField(max_length=32, help_text="Enter the first name")
+    email = models.EmailField(max_length=254, help_text="Enter the email")
+    credits = models.IntegerField()
+
+    def __str__(self):
+        """String to represent the Client object"""
+        return f"{self.last_name}, {self.first_name}"
+    
+    def get_absolute_url(self):
+        """Returns the URL to access a specific client"""
+        return reverse("client-detail-view", args=[str(self.id)])
+
+
+class ClassType(models.Model):
+    """Model representing a type of class."""
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    maximum_class_size = models.IntegerField()
+    credit_cost = models.IntegerField()
+
+    def __str__(self):
+        """String to represent a class type"""
+        return self.title
+    
+    def get_absolute_url(self):
+        """Returns the URL to access a specific class type"""
+        return reverse("classtype-detail-view", args=[str(self.id)])
+
+
+class ClassSession(models.Model):
+    """Model representing a specific session of a class."""
+    class_type = models.ForeignKey(ClassType, on_delete=models.PROTECT)
+    date_and_time = models.DateTimeField(default=timezone.now)
+    roster = models.ManyToManyField(Client)
