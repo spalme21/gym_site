7e668e8c28aa08b95f34d99bb0e47d919dc24a2a
spalme21 <84144056+spalme21@users.noreply.github.com>
2023-05-18 15:49:18 +0000

    Add logout, update sidebar, make login required

--- roster/views.py
+++ roster/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Client
 
@@ -9,25 +10,25 @@ def home(request):
     """View function for the home page."""
     return render(request, "home.html")
 
-class ClientListView(ListView):
+class ClientListView(LoginRequiredMixin, ListView):
     """Generic view for a list of clients."""
     model = Client
 
-class ClientDetailView(DetailView):
+class ClientDetailView(LoginRequiredMixin, DetailView):
     """Generic view to view details about a client."""
     model = Client
 
-class ClientCreateView(CreateView):
+class ClientCreateView(LoginRequiredMixin, CreateView):
     """Generic view to create a new client."""
     model = Client
     fields = ["last_name", "first_name", "email"]
 
-class ClientUpdateView(UpdateView):
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
     """Generic view to update an existing client."""
     model = Client
     fields = ["last_name", "first_name", "email"]
 
-class ClientDeleteView(DeleteView):
+class ClientDeleteView(LoginRequiredMixin, DeleteView):
     """Generic view to delete a client."""
     model = Client
     success_url = reverse_lazy("client-list")
