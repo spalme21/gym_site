eaaee0fa7ee61a00b487509dd3c2a2ba1b38010e
spalme21 <spalme21@gmail.com>
2023-05-19 11:18:13 -0500

    Fix template errors, Add links between pages

--- roster/urls.py
+++ roster/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("clients/", ClientListView.as_view(), name="client-list"),
     path("clients/add/", ClientCreateView.as_view(), name="client-add"),
     path("clients/<int:pk>", ClientDetailView.as_view(), name="client-detail-view"),
-    path("clients/<int:pk>/update/", ClientUpdateView.as_view(), name="client-update"),
-    path("clients/<int:pl>/delete/", ClientDeleteView.as_view(), name="client-delete"),
+    path("clients/<int:pk>/update/", ClientUpdateView.as_view(), name="client-update-view"),
+    path("clients/<int:pk>/delete/", ClientDeleteView.as_view(), name="client-delete-view"),
 ]
